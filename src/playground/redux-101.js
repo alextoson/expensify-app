3dee46d8084fc3d7984889104c8f419719956435
alextoson <alex@toson.com.au>
2020-01-30 17:49:11 +1100

    explored dynamic actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,20 @@ import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
+      const incrementBy =
+        typeof action.incrementBy === "number" ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case "DECREMENT":
+      const decrementBy =
+        typeof action.decrementBy === "number" ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case "SET":
+      return {
+        count: action.count
       };
     case "RESET":
       return {
@@ -19,14 +27,17 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-  type: "INCREMENT"
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
 
 store.dispatch({
-  type: "DECREMENT"
+  type: "INCREMENT",
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: "INCREMENT"
 });
 
 store.dispatch({
@@ -37,4 +48,12 @@ store.dispatch({
   type: "DECREMENT"
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: "DECREMENT",
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: "SET",
+  count: 101
+});
