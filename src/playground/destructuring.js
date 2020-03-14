881e9b981944486243dadbb19dbe8bfc775a328c
alextoson <alex@toson.com.au>
2020-03-14 11:04:54 +1100

    Started to explore ES6 array destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+// Object Destructuring
+//
+
 // const person = {
 //   name: "Alex",
 //   age: 26,
@@ -15,13 +19,25 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: "Ego is the Enemy",
-  Author: "Ryan Holiday",
-  publisher: {
-    name: "Penguin"
-  }
-};
+// const book = {
+//   title: "Ego is the Enemy",
+//   Author: "Ryan Holiday",
+//   publisher: {
+//     name: "Penguin"
+//   }
+// };
 
-const { name: publisherName = "Self-Published" } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = "Self-Published" } = book.publisher;
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+const address = ["1299 S Juniper Street", "Sydney", "NSW", "2009"];
+const [, city, state = "QLD"] = address;
+console.log(`You are in ${city} ${state}`);
+
+const item = ["coffee (hot)", "$2.00", "$2.55", "$2.75"];
+const [menuItem, , medPrice] = item;
+console.log(`A medium ${menuItem} costs ${medPrice}`);
