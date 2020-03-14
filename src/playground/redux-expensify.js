fee1ba9840dffe5d52068593c088e70a962b4195
alextoson <alex@toson.com.au>
2020-03-14 17:27:17 +1100

    added data to the state object

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,3 +1,19 @@
 import { createStore, combineReducers } from "redux";
 
-const demoState = {};
+const demoState = {
+  expense: [
+    {
+      id: "kjsdvbkjsvbb",
+      description: "January Rent",
+      note: "This was the final payment for that address",
+      amount: 54500,
+      createdAt: 0
+    }
+  ],
+  filters: {
+    text: "rent",
+    sortBy: "date", //date or amount
+    startDate: undefined,
+    endDate: undefined
+  }
+};
