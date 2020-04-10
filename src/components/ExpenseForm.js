d9c34815d5f331c7ed8305a40eab625a54e444aa
alextoson <alex@toson.com.au>
2020-04-10 18:58:06 +1000

    added redux dev tools and also fixed a typo in the store

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -56,7 +56,7 @@ export default class ExpenseForm extends React.Component {
         description: this.state.description,
         amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
-        natoe: this.state.note
+        note: this.state.note
       });
     }
   };
