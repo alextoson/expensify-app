e8cc1e35ca317b55f4b9b20101f24633a8232b4b
alextoson <alex@toson.com.au>
2020-07-07 20:51:36 +1000

    fixed an error with Not found and Dashboard test files

--- src/test/components/ExpenseDashboardPage.test.js
+++ src/test/components/ExpenseDashboardPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import ExpenseDashboardPage from "../../components/ExpenseDashboardPage";
 
-test("should render the Header correctly", () => {
+test("should render the ExpenseDashboardPage correctly", () => {
   var wrapper = shallow(<ExpenseDashboardPage />);
   expect(wrapper).toMatchSnapshot();
 });
--- src/test/components/NotFoundPage.test.js
+++ src/test/components/NotFoundPage.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 import NotFoundPage from "../../components/NotFoundPage";
 
-test("should render the Header correctly", () => {
+test("should render the NotFoundPage correctly", () => {
   var wrapper = shallow(<NotFoundPage />);
   expect(wrapper).toMatchSnapshot();
 });
