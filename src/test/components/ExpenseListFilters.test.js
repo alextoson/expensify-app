afeae6fee849ee0c949c6576188a2df37f218a26
alextoson <alex@toson.com.au>
2020-07-08 21:53:56 +1000

    started to set up test for ExpenseListItem

---
+++ src/test/components/ExpenseListFilters.test.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { ExpenseListFilters } from "../../components/ExpenseListFilters";
+import { filters, altFilters } from "../fixtures/filters";
+
+let setTextFilter, sortByDate, sortbyAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortbyAmount = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  wrapper = shallow(
+    <ExpenseListFilters
+      filters={filters}
+      setTextFilter={setTextFilter}
+      sortByDate={sortByDate}
+      sortbyAmount={sortbyAmount}
+      setStartDate={setStartDate}
+      setEndDate={setEndDate}
+    />
+  );
+});
+
+test("should render ExpenseListFilters correctly", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should render ExpenseListFilters with alt data correctly", () => {
+  wrapper.setProps({
+    filters: altFilters,
+  });
+  expect(wrapper).toMatchSnapshot();
+});
