31c543230ec2262f5f5c37a670ba4443e0e136a1
alextoson <alex@toson.com.au>
2020-08-12 22:20:25 +1000

    adding array data to firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,26 +14,80 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-const onValueChange = database.ref().on(
-  "value",
-  (snapshot) => {
-    const data = snapshot.val();
-    console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
-  },
-  (e) => {
-    console.log("Error with data fetching", e);
-  }
-);
+database.ref("expenses").push({
+  description: "Rent",
+  amount: "67000",
+  note: "",
+  createdAt: "515151515",
+});
 
-setTimeout(() => {
-  database.ref().update({
-    name: "Ben",
-  });
-}, 3500);
+database.ref("expenses").push({
+  description: "Food",
+  amount: "7000",
+  note: "",
+  createdAt: "51515351",
+});
 
-setTimeout(() => {
-  database.ref().off("value", onValueChange);
-}, 7000);
+database.ref("expenses").push({
+  description: "Petrol",
+  amount: "1020",
+  note: "",
+  createdAt: "241354645",
+});
+
+// database.ref("notes").push({
+//   title: "Course Topics",
+//   body: "react, monogo",
+// });
+
+// const firebaseNotes = {
+//   notes: {
+//     hacvmsnvcas: {
+//       title: "first note",
+//       body: "this is my note",
+//     },
+//     kjhschvwec: {
+//       title: "Another note",
+//       body: "this is my note",
+//     },
+//   },
+// };
+
+// const notes = [
+//   {
+//     id: "12",
+//     title: "first note",
+//     body: "this is my note",
+//   },
+//   {
+//     id: "75427",
+//     title: "Another note",
+//     body: "this is my note",
+//   },
+// ];
+
+// database.ref("notes").set(firebaseNotes);
+
+// const onValueChange = database.ref().on(
+//   "value",
+//   (snapshot) => {
+//     const data = snapshot.val();
+//     console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+//   },
+//   (e) => {
+//     console.log("Error with data fetching", e);
+//   }
+// );
+
+// setTimeout(() => {
+//   database.ref().update({
+//     name: "Ben",
+//   });
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off("value", onValueChange);
+// }, 7000);
 
 // database
 //   .ref("location/city")
