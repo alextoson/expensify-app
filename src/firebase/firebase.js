38053df2102b2107758df513ce0a82ce1d9985d2
alextoson <alex@toson.com.au>
2020-09-03 18:29:40 +1000

    set up redux thunk middle ware and connected the app to firebase and successfully created an entry through the interface

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,155 +14,157 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-// child_removed
-database.ref("expenses").on(
-  "child_removed",
-  (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  },
-  (e) => {
-    console.log("Error with data fetching", e);
-  }
-);
+export { firebase, database as default };
 
-// child_changed
-database.ref("expenses").on(
-  "child_changed",
-  (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  },
-  (e) => {
-    console.log("Error with data fetching", e);
-  }
-);
-
-// child_added
-database.ref("expenses").on(
-  "child_added",
-  (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  },
-  (e) => {
-    console.log("Error with data fetching", e);
-  }
-);
-
+// // child_removed
 // database.ref("expenses").on(
-//   "value",
+//   "child_removed",
 //   (snapshot) => {
-//     const expenses = [];
-//     snapshot.forEach((childSnapshot) => {
-//       expenses.push({
-//         id: childSnapshot.key,
-//         ...childSnapshot.val(),
-//       });
-//     });
-//     console.log(expenses);
+//     console.log(snapshot.key, snapshot.val());
 //   },
 //   (e) => {
 //     console.log("Error with data fetching", e);
 //   }
 // );
 
-// database.ref("notes").push({
-//   title: "Course Topics",
-//   body: "react, monogo",
-// });
-
-// const firebaseNotes = {
-//   notes: {
-//     hacvmsnvcas: {
-//       title: "first note",
-//       body: "this is my note",
-//     },
-//     kjhschvwec: {
-//       title: "Another note",
-//       body: "this is my note",
-//     },
-//   },
-// };
-
-// const notes = [
-//   {
-//     id: "12",
-//     title: "first note",
-//     body: "this is my note",
-//   },
-//   {
-//     id: "75427",
-//     title: "Another note",
-//     body: "this is my note",
-//   },
-// ];
-
-// database.ref("notes").set(firebaseNotes);
-
-// const onValueChange = database.ref().on(
-//   "value",
+// // child_changed
+// database.ref("expenses").on(
+//   "child_changed",
 //   (snapshot) => {
-//     const data = snapshot.val();
-//     console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+//     console.log(snapshot.key, snapshot.val());
 //   },
 //   (e) => {
 //     console.log("Error with data fetching", e);
 //   }
 // );
 
-// setTimeout(() => {
-//   database.ref().update({
-//     name: "Ben",
-//   });
-// }, 3500);
+// // child_added
+// database.ref("expenses").on(
+//   "child_added",
+//   (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   },
+//   (e) => {
+//     console.log("Error with data fetching", e);
+//   }
+// );
 
-// setTimeout(() => {
-//   database.ref().off("value", onValueChange);
-// }, 7000);
+// // database.ref("expenses").on(
+// //   "value",
+// //   (snapshot) => {
+// //     const expenses = [];
+// //     snapshot.forEach((childSnapshot) => {
+// //       expenses.push({
+// //         id: childSnapshot.key,
+// //         ...childSnapshot.val(),
+// //       });
+// //     });
+// //     console.log(expenses);
+// //   },
+// //   (e) => {
+// //     console.log("Error with data fetching", e);
+// //   }
+// // );
 
-// database
-//   .ref("location/city")
-//   .once("value")
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   })
-//   .catch((error) => {
-//     console.log("Error fetching data", error);
-//   });
+// // database.ref("notes").push({
+// //   title: "Course Topics",
+// //   body: "react, monogo",
+// // });
 
-// database
-//   .ref()
-//   .set({
-//     name: "Alex Toson",
-//     age: 27,
-//     stressLevel: 6,
-//     location: {
-//       city: "Sydney",
-//       country: "Australia",
-//     },
-//     job: {
-//       title: "Software Engineer",
-//       company: "New Republique",
-//     },
-//   })
-//   .then(() => {
-//     console.log("Data is saved!");
-//   })
-//   .catch((error) => {
-//     console.log("this failed", error);
-//   });
+// // const firebaseNotes = {
+// //   notes: {
+// //     hacvmsnvcas: {
+// //       title: "first note",
+// //       body: "this is my note",
+// //     },
+// //     kjhschvwec: {
+// //       title: "Another note",
+// //       body: "this is my note",
+// //     },
+// //   },
+// // };
 
-// database.ref().update({
-//   stressLevel: 9,
-//   "job/company": "Atlassian",
-//   "location/city": "Melbourne",
-// });
+// // const notes = [
+// //   {
+// //     id: "12",
+// //     title: "first note",
+// //     body: "this is my note",
+// //   },
+// //   {
+// //     id: "75427",
+// //     title: "Another note",
+// //     body: "this is my note",
+// //   },
+// // ];
 
-// database
-//   .ref()
-//   .remove()
-//   .then(() => {
-//     console.log("Remove succeeded.");
-//   })
-//   .catch((error) => {
-//     console.log("Remove failed: " + error.message);
-//   });
+// // database.ref("notes").set(firebaseNotes);
+
+// // const onValueChange = database.ref().on(
+// //   "value",
+// //   (snapshot) => {
+// //     const data = snapshot.val();
+// //     console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+// //   },
+// //   (e) => {
+// //     console.log("Error with data fetching", e);
+// //   }
+// // );
+
+// // setTimeout(() => {
+// //   database.ref().update({
+// //     name: "Ben",
+// //   });
+// // }, 3500);
+
+// // setTimeout(() => {
+// //   database.ref().off("value", onValueChange);
+// // }, 7000);
+
+// // database
+// //   .ref("location/city")
+// //   .once("value")
+// //   .then((snapshot) => {
+// //     const val = snapshot.val();
+// //     console.log(val);
+// //   })
+// //   .catch((error) => {
+// //     console.log("Error fetching data", error);
+// //   });
+
+// // database
+// //   .ref()
+// //   .set({
+// //     name: "Alex Toson",
+// //     age: 27,
+// //     stressLevel: 6,
+// //     location: {
+// //       city: "Sydney",
+// //       country: "Australia",
+// //     },
+// //     job: {
+// //       title: "Software Engineer",
+// //       company: "New Republique",
+// //     },
+// //   })
+// //   .then(() => {
+// //     console.log("Data is saved!");
+// //   })
+// //   .catch((error) => {
+// //     console.log("this failed", error);
+// //   });
+
+// // database.ref().update({
+// //   stressLevel: 9,
+// //   "job/company": "Atlassian",
+// //   "location/city": "Melbourne",
+// // });
+
+// // database
+// //   .ref()
+// //   .remove()
+// //   .then(() => {
+// //     console.log("Remove succeeded.");
+// //   })
+// //   .catch((error) => {
+// //     console.log("Remove failed: " + error.message);
+// //   });
