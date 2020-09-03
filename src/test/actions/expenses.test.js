5b46deaee54bb41510a0ec235187039386798380
alextoson <alex@toson.com.au>
2020-09-04 08:29:25 +1000

    started Testing Async Redux Actions

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -1,35 +1,52 @@
-import { addExpense, editExpense, removeExpense } from "../../actions/expenses";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import {
+  addExpense,
+  editExpense,
+  removeExpense,
+  startAddExpense,
+} from "../../actions/expenses";
+import expenses from "../fixtures/expenses";
+
+const createMockStore = configureMockStore([thunk]);
 
 test("should setup add expense action object with provided values", () => {
-  const expenseData = {
-    description: "Rent",
-    amount: 109500,
-    createdAt: 1000,
-    note: "This was last months reant",
-  };
-  const action = addExpense(expenseData);
+  const action = addExpense(expenses[2]);
   expect(action).toEqual({
     type: "ADD_EXPENSE",
-    expense: {
-      ...expenseData,
-      id: expect.any(String),
-    },
+    expense: expenses[2],
   });
 });
 
-test("should setup add expense action object with default values", () => {
-  const action = addExpense();
-  expect(action).toEqual({
-    type: "ADD_EXPENSE",
-    expense: {
-      id: expect.any(String),
-      description: "",
-      note: "",
-      amount: 0,
-      createdAt: 0,
-    },
+test("should add expense to database and store", (done) => {
+  const store = createMockStore({});
+  const expenseData = {
+    description: "mouse",
+    amount: 3000,
+    note: "I like this mouse",
+    createAt: 1000,
+  };
+  store.dispatch(startAddExpense(expenseData)).then(() => {
+    expect(1).toBe(1);
+    done();
   });
 });
+
+test("should add expense with defaults to database and store", () => {});
+
+// test("should setup add expense action object with default values", () => {
+//   const action = addExpense();
+//   expect(action).toEqual({
+//     type: "ADD_EXPENSE",
+//     expense: {
+//       id: expect.any(String),
+//       description: "",
+//       note: "",
+//       amount: 0,
+//       createdAt: 0,
+//     },
+//   });
+// });
 
 test("should setup edit expense action object", () => {
   const action = editExpense("123abc", { note: "New note value" });
