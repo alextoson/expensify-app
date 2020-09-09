d88ca131d0ea88d7da63f6b404fb5473f406351e
alextoson <alex@toson.com.au>
2020-09-09 21:13:37 +1000

    setup firebase fetch and test

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -6,6 +6,7 @@ import {
   removeExpense,
   startAddExpense,
   setExpenses,
+  startSetExpenses,
 } from "../../actions/expenses";
 import expenses from "../fixtures/expenses";
 import database from "../../firebase/firebase";
@@ -133,5 +134,17 @@ test("should set up expense action object with data", () => {
   expect(action).toEqual({
     type: "SET_EXPENSES",
     expenses,
+  });
+});
+
+test("should fecth teh expenses from firebase", (done) => {
+  const store = createMockStore({});
+  store.dispatch(startSetExpenses()).then(() => {
+    const actions = store.getActions();
+    expect(actions[0]).toEqual({
+      type: "SET_EXPENSES",
+      expenses,
+    });
+    done();
   });
 });
