c97f668fe5241f5f1b6b3f6b8f73070e12d1ab29
alextoson <alex@toson.com.au>
2020-09-12 16:39:52 +1000

    added logout functionality

--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Header from "../../components/Header";
+import { Header } from "../../components/Header";
 
 test("should render the Header correctly", () => {
-  var wrapper = shallow(<Header />);
+  var wrapper = shallow(<Header startLogout={() => {}} />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test("should call startLogout on button click", () => {
+  const onLogoutSpy = jest.fn();
+  var wrapper = shallow(<Header startLogout={onLogoutSpy} />);
+  wrapper.find("button").simulate("click");
+  expect(onLogoutSpy).toHaveBeenCalled();
 });
--- src/test/components/LoginPage.test.js
+++ src/test/components/LoginPage.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { shallow } from "enzyme";
-import LoginPage from "../../components/LoginPage";
+import { LoginPage } from "../../components/LoginPage";
 
 test("should render the LoginPage correctly", () => {
   var wrapper = shallow(<LoginPage />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test("should call startLogin on button click", () => {
+  const onLoginSpy = jest.fn();
+  var wrapper = shallow(<LoginPage startLogin={onLoginSpy} />);
+  wrapper.find("button").simulate("click");
+  expect(onLoginSpy).toHaveBeenCalled();
 });
